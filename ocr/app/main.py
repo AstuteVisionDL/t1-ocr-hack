26d5a8be7f30688a9acf4e8ceab649baa6f9e62d
Max <makstislenko@gmail.com>
2024-10-26 14:36:34 +0400

    Pipeline

--- ocr/app/main.py
+++ ocr/app/main.py
@@ -1,7 +1,18 @@
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
+import os
+import io
+import cv2
+import numpy as np
+from PIL import Image
+from surya.model.detection.model import load_model as load_det_model, load_processor as load_det_processor
+from surya.model.recognition.model import load_model as load_rec_model
+from surya.model.recognition.processor import load_processor as load_rec_processor
+from surya.ocr import run_ocr
+from surya.schema import OCRResult
 
 app = FastAPI()
 
@@ -14,6 +25,19 @@ app.add_middleware(
     expose_headers=["Content-Disposition"],
 )
 
+def process_single_image(image) -> list[OCRResult]:
+    """
+    Main function of OCR application
+    :param image: image with sheet of paper
+    :return: text on the image
+    """
+
+
+    langs = ["ru"]
+    det_processor, det_model = load_det_processor(), load_det_model()
+    rec_model, rec_processor = load_rec_model(), load_rec_processor()
+    predictions = run_ocr([image], [langs], det_model, det_processor, rec_model, rec_processor)
+    return predictions
 
 class DocumentResponse(BaseModel):
     coordinates: list[list[int]]
@@ -22,20 +46,15 @@ class DocumentResponse(BaseModel):
     signature: bool
 
 
-@app.post("/upload-document/", response_model=list[DocumentResponse])
+@app.post("/upload-document/", response_model=list[OCRResult])
 async def upload_document(file: UploadFile = File(...)):
-    response_data = [
-        {
-            "coordinates": [[0, 0], [10, 10]],
-            "content": "text",
-            "language": "rus",
-            "signature": False,
-        },
-        {
-            "coordinates": [[10, 10], [20, 20]],
-            "content": "",
-            "language": "",
-            "signature": True,
-        },
-    ]
-    return JSONResponse(content=response_data)
+    response_data = []
+    contents = await file.read()
+
+    # Open the image using PIL from the bytes
+    image = Image.open(io.BytesIO(contents))
+    result = process_single_image(image)
+    return JSONResponse(content=result)
+
+if __name__=='__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
